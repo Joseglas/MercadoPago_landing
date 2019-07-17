76f59799856e144ea7ebd27347b19b9d4b20962e
Jose Glas <39164416+Joseglas@users.noreply.github.com>
2019-07-17 15:11:48 -0300

    1
    First Commit

---
+++ viewer/SymbolViewer.js
@@ -0,0 +1,172 @@
+class SymbolViewer{
+    constructor (){
+        this.visible = false
+        this.createdPages = {}
+    }
+
+    initialize(){
+        
+    }
+
+
+    toggle(){
+        return this.visible ? this.hide(): this.show()        
+    }
+
+    hide(){
+        if(viewer.currentPageModal){
+            $(".modalSymbolLink").remove()
+            delete this.createdPages[viewer.currentPage]
+        }
+        const contentDiv = viewer.currentPageModal?  $('#content-modal'): $('#content')
+        contentDiv.removeClass("contentSymbolsVisible")        
+
+        this.visible = false
+
+        // hide sidebar
+        viewer.sidebarVisible=false
+        $('#sidebar').addClass("hidden")
+        $('#symbol_viewer').addClass("hidden")
+        viewer.zoomContent()
+    }
+
+    show(){
+        
+        viewer.toggleLinks(false)
+        viewer.toogleLayout(false)
+        
+        this._showPage(viewer.currentPage)
+        if(this.page.currentOverlayPage){
+            this._showPage(this.page.currentOverlayPage.index)
+        }
+
+        const contentDiv = viewer.currentPageModal?  $('#content-modal'): $('#content')
+        contentDiv.addClass("contentSymbolsVisible")
+ 
+        // show sidebar
+        viewer.sidebarVisible=true
+        $('#symbol_viewer').removeClass("hidden")        
+        $('#sidebar').removeClass("hidden")
+        viewer.zoomContent()
+
+        this.visible = true
+    }
+
+    _showPage(pageIndex){
+        this.pageIndex = pageIndex
+        this.page = story.pages[pageIndex];        
+        if(!(pageIndex in this.createdPages)){
+            const newPageInfo = {
+                layerArray:[]
+            }
+            // cache only standalone pages
+            // if(!viewer.currentPageModal){                  
+             this.createdPages[pageIndex] = newPageInfo
+            
+            this.pageInfo = newPageInfo
+            this._create()           
+        }else{
+            this.pageInfo = this.createdPages[pageIndex]
+        }
+    }
+
+
+
+    _create(){        
+        this._processLayerList(layersData[this.pageIndex].childs)        
+    }
+
+    _processLayerList(layers,isParentSymbol=false){
+        for(var l of layers){
+            if(l.symbolMasterName!=undefined || (!isParentSymbol && l.styleName!=undefined)){
+                this._showElement(l)
+            }
+            this._processLayerList(l.childs,l.symbolMasterName!=undefined)
+        }
+    }
+
+    _showElement(l){
+
+        var currentPanel = this.page
+    
+        for(const panel of this.page.fixedPanels){
+            if( l.frame.x >= panel.x && l.frame.y >= panel.y &&
+                ((l.frame.x + l.frame.width) <= (panel.x + panel.width )) && ((l.frame.y + l.frame.height) <= (panel.y + panel.height ))
+            ){
+                currentPanel = panel
+                break
+            }
+        }
+
+        const layerIndex = this.pageInfo.layerArray.length
+        this.pageInfo.layerArray.push(l)
+
+        var a = $("<a>",{
+            class:      viewer.currentPageModal?"modalSymbolLink":"symbolLink",
+            pi:         this.pageIndex,
+            li:         layerIndex,
+        })        
+
+        a.click(function () {
+            const pageIndex =  $( this ).attr("pi")
+            const layerIndex =  $( this ).attr("li")
+            const layer = viewer.symbolViewer.createdPages[pageIndex].layerArray[layerIndex]
+            
+            var symName = layer.symbolMasterName
+            var styleName = layer.styleName
+            var comment = layer.comment
+            var frameX = layer.frame.x
+            var frameY = layer.frame.y
+            var frameWidth = layer.frame.width
+            var frameHeight = layer.frame.height
+
+            var info = ""
+            if(symName!=undefined) info = "<p class='head'>Symbol</p>"+symName
+            if(styleName!=undefined) info = "<p class='head'>Style</p> "+styleName
+            
+            if(comment!=undefined) info += "<p class='head'>Comment</p> "+comment
+
+            info += "<p class='head'>Position (left x top)</p>" + frameX + " x " + frameY
+            info += "<p class='head'>Size (width x height)</p>" + frameWidth + " x " + frameHeight
+
+            if(layer.text!=undefined && layer.text!=''){
+                info+="<p class='head'>Text</p> "+layer.text
+            }
+
+            if(symName!=undefined && symName in symbolsData){
+                const symInfo = symbolsData[symName]
+                info+="<p class='head'>Symbol layers and Tokens</p>"
+                var layerCounter = 0
+                for(const layerName of Object.keys(symInfo.layers)){
+                    if(layerCounter)
+                        info+="<br/>"    
+                    info+=layerName + "<br/>"
+                    for(const tokenName of Object.keys(symInfo.layers[layerName].tokens)){
+                        info+=tokenName+"<br/>"
+                    }
+                    layerCounter++
+                }                
+            }
+            if(styleName!=undefined && styleName in  symbolsData.styles){
+                const styleInfo = symbolsData.styles[styleName]
+                info+="<p class='head'>Style Tokens</p>"     
+                for(const tokenName of Object.keys(styleInfo.tokens)){
+                    info+=tokenName+"<br/>"
+                }                                
+            }
+            
+            $("#symbol_viewer_content").html(info)
+            //alert(info)
+        })
+
+        a.appendTo(currentPanel.linksDiv)
+
+        var style="left: "+ l.frame.x+"px; top:"+l.frame.y+"px; width: " + l.frame.width + "px; height:"+l.frame.height+"px; "
+        var symbolDiv = $("<div>",{
+            class:"symbolDiv",
+        }).attr('style', style)
+                    
+        symbolDiv.appendTo(a) 
+
+    }
+}
---
+++ viewer/story.js
@@ -0,0 +1,113 @@
+var story = {
+"docName": "mercadopago​-​pos-landing",
+"docPath": "P_P_P",
+"docVersion": "V_V_V",
+"hasRetina": true,
+"disableHotspots": true,
+"pages": [
+$.extend(new ViewerPage(),{
+"index": 0,
+"image": "landing_.png",
+"image2x": "landing_@2x.png",
+"width": 1366,
+"height": 3815,
+"title": "Landing ",
+'layout' : {
+	"offset": 0,
+	"totalWidth": 960,
+	"numberOfColumns": 12,
+	"columnWidth": 60,
+	"gutterWidth": 20
+},
+'type': 'regular',
+'fixedPanels': [
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 0,
+		"width": 1366,
+		"height": 80,
+		"type": "float",
+		"index": 0,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [],
+		"image": "landing_-0.png",
+		"image2x": "landing_-0@2x.png",
+		"shadow": "0px 0px 8px 0 #0000001a ",
+		"shadowX": 8
+	}
+],
+'links' : [],
+})
+,$.extend(new ViewerPage(),{
+"index": 1,
+"image": "colors.png",
+"image2x": "colors@2x.png",
+"width": 311,
+"height": 210,
+"title": "Colors",
+'layout' : {
+	"offset": 0,
+	"totalWidth": 960,
+	"numberOfColumns": 12,
+	"columnWidth": 60,
+	"gutterWidth": 20
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [],
+})
+,$.extend(new ViewerPage(),{
+"index": 2,
+"image": "greys.png",
+"image2x": "greys@2x.png",
+"width": 248,
+"height": 174,
+"title": "Greys",
+'layout' : {
+	"offset": 0,
+	"totalWidth": 960,
+	"numberOfColumns": 12,
+	"columnWidth": 60,
+	"gutterWidth": 20
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [],
+})
+,$.extend(new ViewerPage(),{
+"index": 3,
+"image": "fonts.png",
+"image2x": "fonts@2x.png",
+"width": 865,
+"height": 174,
+"title": "fonts",
+'layout' : {
+	"offset": 0,
+	"totalWidth": 960,
+	"numberOfColumns": 12,
+	"columnWidth": 60,
+	"gutterWidth": 20
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [],
+})
+   ]
+,"resolutions": [2],
+"zoomEnabled": true,
+"title": "MercadoPago​ ​POS Landing",
+"layersExist": true,
+"centerContent":  false,
+"totalImages": 5,
+"highlightLinks": false
+}
